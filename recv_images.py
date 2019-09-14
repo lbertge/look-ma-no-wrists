2ef67dcf498822664f4814d24dac4a918b352a4c
Albert Ge <lbertge@gmail.com>
2019-09-14 15:29:12 -0700

    setup image hub

---
+++ recv_images.py
@@ -0,0 +1,27 @@
+import sys
+from pytorch_ssd.vision.ssd.mobilenetv1_ssd import create_mobilenetv1_ssd, create_mobilenetv1_ssd_predictor
+import cv2
+sys.path.insert(0, '/imagezmq/imagezmq')
+import imagezmq
+
+
+top_k = 2
+threshold = 0.7
+
+def predict(image):
+    net = create_mobilenetv1_ssd(top_k, is_test=True)
+    predictor = create_mobilenetv1_ssd_predictor(net, candidate_size=200)
+
+    boxes, labels, probs = predictor.predict(image, top_k, threshold)
+    if boxes.size(0) > 0:
+        return boxes, labels, probs
+    return None
+
+if __name__ == '__main__':
+    image_hub = imagezmq.ImageHub()
+    while True:
+        image_name, image = image_hub.recv_image()
+        print(image_name, image.shape)
+        image_hub.send_reply(b'OK')
+
+
