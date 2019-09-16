37e7c212b491984c679dfd04debc5a444f9d3ecc
Albert Ge <lbertge@gmail.com>
2019-09-16 13:23:41 -0700

    update recv_images serialize numpy data

--- recv_images.py
+++ recv_images.py
@@ -5,25 +5,30 @@ import cv2
 sys.path.insert(0, 'imagezmq/imagezmq')
 import imagezmq
 import numpy as np
-
+import argparse
+import zmqnumpy
 
 top_k = 2
 threshold = 0.7
 
-def predict(image):
+if __name__ == '__main__':
+    image_hub = imagezmq.ImageHub()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', dest='model_path', type=str, help="Trained model path", required=True)
+    args = parser.parse_args()
+
     net = create_mobilenetv1_ssd(top_k, is_test=True)
     predictor = create_mobilenetv1_ssd_predictor(net, candidate_size=200)
 
-    boxes, labels, probs = predictor.predict(image, top_k, threshold)
-    if boxes.size(0) > 0:
-        return boxes, labels, probs
-    return None
+    net.load(args.model_path)
 
-if __name__ == '__main__':
-    image_hub = imagezmq.ImageHub()
     while True:
         image_name, image = image_hub.recv_image()
-        out = predict(image)
-        image_hub.send_reply(out)
+        boxes, labels, probs = predictor.predict(image, top_k, threshold)
+        if boxes.size(0) > 0:
+            loc = [boxes[i, :].numpy() for i in range(boxes.size(0))]
+            image_hub.send_reply(np.array(loc))
+        else:
+            image_hub.send_reply(b'')
 
 
